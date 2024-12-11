b1b64bc758a6ce58133bd94b702b7e72059acfb2
estelle <estelle_mgd>
2024-12-11 11:56:21 +0100

    corrections

--- src/maze.rs
+++ src/maze.rs
@@ -1,4 +1,5 @@
 use Maze::*;
+use Exploration::*;
 
 enum Exploration {
     Explored,
@@ -10,43 +11,45 @@ enum Maze<'a> {
         label: String,
         left: &'a Maze<'a>,
         right: &'a Maze<'a>,
-        //status: Exploration,  // À ajouter une fois Exploration implémentée
+        status: Exploration,
     },
     Leaf {
         label: String,
     },
 }
 
-impl Maze< > {
+impl<'a> Maze<'a> {
 
-    fn newLeaf(label: String) -> Maze< > {Leaf { label: label } }
+    fn newLeaf(label: String) -> Self {Leaf { label: label } }
 
-    fn newBranch(label: String, left: & Maze < >, right: & Maze < >) -> Maze< > {Branch { label: label, left: left, right: right } }
+    fn newBranch(label: String, left: &'a Maze <'a>, right: &'a Maze <'a>) -> Self {
+        Branch { label, left, right, status: UnExplored }
+    }
 
 }
 
-impl<'a> Maze<'a> {
+/*impl<'a> Maze<'a> {
     fn explore(&self, trace: &mut Vec<String>) {
         match self {
             Branch {label, left, right} => {trace.push(label.clone()); left.explore(trace); right.explore(trace); }
             Leaf { label } => {trace.push(label.clone()); }
         }
     }
-}
+}*/
 
 pub fn main() {
     let leaf2 = Maze::Leaf {label: format!("2")};
     let leaf4 = Maze::Leaf {label: format!("4")};
     let leaf5 = Maze::Leaf {label: format!("5")};
     let leaf8 = Maze::Leaf {label: format!("8")};
-    let branch3 = Maze::Branch{label: format!("3"), left: &leaf4, right: &leaf5};
-    let branch1 = Maze::Branch{label: format!("1"), left: &leaf2, right: &branch3};
-    let branch7 = Maze::Branch{label: format!("7"), left: &leaf5, right: &leaf8};
-    let branch6 = Maze::Branch{label: format!("6"), left: &branch3, right: &branch7};
-    let branch0 = Maze::Branch{label: format!("0"), left: &branch1, right: &branch6};
+    let branch3 = Maze::newBranch(format!("3"), &leaf4, &leaf5);
+    let branch1 = Maze::newBranch(format!("1"), &leaf2, &branch3);
+    let branch7 = Maze::newBranch(format!("7"), &leaf5, &leaf8);
+    let branch6 = Maze::newBranch(format!("6"), &branch3, &branch7);
+    let branch0 = Maze::newBranch(format!("0"), &branch1, &branch6);
     println!("labyrinthe créé");
 
     let mut trace: Vec<String> = Vec::new();
-    branch0.explore(&mut trace);
-    println!("{:?}", trace);
+    //branch0.explore(&mut trace);
+    //println!("{:?}", trace);
 }
