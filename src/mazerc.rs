03afecde5695c2f5293d5e4d40a9aaaddfd3ba32
estelle <estelle_mgd>
2024-12-15 20:24:29 +0100

    fonctionne comme exemple avec maze encapsulée

--- src/mazerc.rs
+++ src/mazerc.rs
@@ -1,77 +1,3 @@
-/*use std::rc::Rc;
-
-enum Exploration {
-    Explored,
-    UnExplored,
-}
-
-enum Maze {
-    Branch {
-        label: String,
-        left: Rc<Maze>,
-        right: Rc<Maze>,
-        status: Exploration,
-    },
-    Leaf {
-        label: String,
-    },
-}
-
-impl Maze {
-    fn newLeaf(label: String) -> Self {
-        Maze::Leaf { label }
-    }
-
-    fn newBranch(label: String, left: Rc<Maze>, right: Rc<Maze>) -> Self {
-        Maze::Branch {
-            label,
-            left,
-            right,
-            status: Exploration::UnExplored,
-        }
-    }
-
-    /*fn explore(&mut self) -> Vec<String> {
-        match self {
-            Maze::Branch { label, left, right, status } => {
-                match status {
-                    Exploration::UnExplored => {
-                        *status = Exploration::Explored;
-                        let mut result = vec![label.clone()];
-                        result.extend(left.explore());
-                        result.extend(right.explore());
-                        result
-                    }
-                    Exploration::Explored => vec![label.clone()],
-                }
-            }
-            Maze::Leaf { label } => vec![label.clone()],
-        }
-    }*/
-}
-
-fn maze() -> Rc<Maze> {
-    let leaf2 = Rc::new(Maze::Leaf { label: format!("2") });
-    let leaf4 = Rc::new(Maze::Leaf { label: format!("4") });
-    let leaf5 = Rc::new(Maze::Leaf { label: format!("5") });
-    let leaf8 = Rc::new(Maze::Leaf { label: format!("8") });
-
-    let branch3 = Rc::new(Maze::newBranch(format!("3"), Rc::clone(&leaf4), Rc::clone(&leaf5)));
-    let branch1 = Rc::new(Maze::newBranch(format!("1"), Rc::clone(&leaf2), Rc::clone(&branch3)));
-    let branch7 = Rc::new(Maze::newBranch(format!("7"), Rc::clone(&leaf5), Rc::clone(&leaf8)));
-    let branch6 = Rc::new(Maze::newBranch(format!("6"), Rc::clone(&branch3), Rc::clone(&branch7)));
-    Rc::new(Maze::newBranch(format!("0"), Rc::clone(&branch1), Rc::clone(&branch6)))
-}
-
-/*pub fn main() {
-    let mut labyrinthe = maze();
-    println!("Labyrinthe créé");
-
-    let trace = labyrinthe.explore();
-    println!("{:?}", trace);
-}
-*/*/
-
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -144,7 +70,7 @@ impl Maze {
                             *status_mut = Exploration::Explored;
                         }
                         Maze::explore(right.clone(), stack, {
-                            labels.push(label.clone());
+                            //labels.push(label.clone());
                             labels
                         })
                     }
@@ -173,7 +99,7 @@ fn loop_explore(pair: (Vec<Rc<RefCell<Maze>>>, Vec<String>)) -> (Vec<Rc<RefCell<
     (stack, labels)
 }
 
-pub fn main() {
+fn maze() -> Rc<RefCell<Maze>> {
     let leaf2 = Maze::new_leaf("2");
     let leaf4 = Maze::new_leaf("4");
     let leaf5 = Maze::new_leaf("5");
@@ -185,6 +111,12 @@ pub fn main() {
     let branch6 = Maze::new_branch("6", branch3.clone(), branch7.clone());
     let branch0 = Maze::new_branch("0", branch1.clone(), branch6.clone());
 
-    let (_, labels) = loop_explore((vec![branch0.clone()], vec![]));
-    println!("Explored nodes: {:?}", labels);
+    branch0
 }
+
+pub fn main() {
+        let root = maze();
+
+        let (_, labels) = loop_explore((vec![root.clone()], vec![]));
+        println!("Explored nodes: {:?}", labels);
+    }
