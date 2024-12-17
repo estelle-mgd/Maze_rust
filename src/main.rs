f967c6b4600b69fab42f1523030c4c25f324b6b4
estelle <estelle_mgd>
2024-12-17 11:48:48 +0100

    main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 mod maze;
+mod mazerc;
+mod mazepile;
+mod mazeconc;
+mod maze2leaf;
 
 fn main() {
     println!("Hello, world!");
+    println!("Maze avec des références TP 3 s3");
     maze::main();
+    println!("Maze avec des pointeurs Rc TP 3 s4");
+    mazerc::main();
+    println!("Maze avec une pile d'exploration TP 3 s5");
+    mazepile::main();
+    println!("Maze version concurrente TP 4 s1.2");
+    mazeconc::main();
+    println!("Maze version concurrente avec arrêt à la sortie TP 4 s2");
+    maze2leaf::main();
 }
